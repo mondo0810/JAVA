0c4f15b88c5b02fb04a27b0dbb0d336c6f5f4f35
DoThi <thidovan58@gmail.com>
2023-09-07 23:03:17 +0700

    fd

---
+++ LAB/src/lab1/main.java
@@ -0,0 +1,7 @@
+package lab1;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world lab1!");
+    }
+
+}
---
+++ LAB/src/lab2/main.java
@@ -0,0 +1,13 @@
+package lab2;
+public class main {
+    public static void main(String[] args) {
+        Student studentA;
+        Student studentB;
+        studentA=new Student();
+        studentB=new Student(1,"Do Van Thi",true);
+
+        studentB.printInfo();
+
+    }
+
+}
