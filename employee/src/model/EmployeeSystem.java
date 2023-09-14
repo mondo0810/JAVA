38e21ceeaf9897ad589231f1ca328f55229519f8
DoThi <thidovan58@gmail.com>
2023-09-14 23:51:55 +0700

    hih

--- employee/src/model/EmployeeSystem.java
+++ employee/src/model/EmployeeSystem.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-
 class CustomException extends Exception {
     private static final long serialVersionUID = 1L;
 
@@ -18,83 +17,106 @@ class CustomException extends Exception {
 public class EmployeeSystem {
     public static Map<Integer, Employee> map = new HashMap<>();
 
-    public static void addEmployee(String name, int age, int employeeId) {
-        Employee emp = new Employee(name, age, employeeId);
-        map.put(employeeId, emp);
+    public static void addEmployee(String name, int age, int employeeId) throws CustomException {
+        try {
+            Employee emp = new Employee(name, age, employeeId);
+            map.put(employeeId, emp);
+            System.out.println("Employee added successfully.");
+        }
+        catch (Exception e) {
+            throw new CustomException("Invalid employee data: " + e.getMessage());
+        }
         operations();
     }
 
-    public static void deleteEmployee(int empId) {
+    public static void deleteEmployee(int empId) throws CustomException {
         if (map.containsKey(empId)) {
             map.remove(empId);
             System.out.println("Employee with ID " + empId + " has been deleted.");
         } else {
-            System.out.println("Employee with ID " + empId + " not found.");
+            throw new CustomException("Employee with ID " + empId + " not found.");
         }
         operations();
     }
 
-    public static void searchEmployee(int empId) {
+    public static void searchEmployee(int empId) throws CustomException {
         if (map.containsKey(empId)) {
             System.out.println("Employee Details: " + map.get(empId));
         } else {
-            System.out.println("Employee with ID " + empId + " not found.");
+            throw new CustomException("Employee with ID " + empId + " not found.");
         }
         operations();
     }
 
-    public static void listEmployees() {
+    public static void listEmployees() throws CustomException {
+        if (map.isEmpty()) {
+            throw new CustomException("No employees found.");
+        }
         System.out.println(map.toString());
         operations();
     }
 
     public static void operations() {
-        System.out.println("\n***** Employee management system ******");
-        System.out.println("1. Add Employee");
-        System.out.println("2. Delete Employee");
-        System.out.println("3. Search Employee");
-        System.out.println("4. Employee List");
-        System.out.println("5. Exit");
+        try {
+            System.out.println("\n***** Employee management system ******");
+            System.out.println("1. Add Employee");
+            System.out.println("2. Delete Employee");
+            System.out.println("3. Search Employee");
+            System.out.println("4. Employee List");
+            System.out.println("5. Exit");
 
-        Scanner scanner = new Scanner(System.in);
-        int userInput = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
 
-        switch (userInput) {
-            case 1:
-                System.out.println("Enter Employee Details (Name, age, id)");
-                Scanner scanner1 = new Scanner(System.in);
+            if (scanner.hasNextInt()) { // Kiểm tra xem người dùng đã nhập một số nguyên hay chưa
+                int userInput = scanner.nextInt();
 
-                String name = scanner1.next();
-                int age = scanner1.nextInt();
-                int id = scanner1.nextInt();
+                switch (userInput) {
+                    case 1:
+                        System.out.println("Enter Employee Details (Name, age, id)");
+                        Scanner scanner1 = new Scanner(System.in);
 
-                if (!name.equals("") && age != 0 && id != 0) {
-                    addEmployee(name, age, id);
+                        String name = scanner1.next();
+                        int age = scanner1.nextInt();
+                        int id = scanner1.nextInt();
+
+                        if (!name.isEmpty() && age != 0 && id != 0) {
+                            addEmployee(name, age, id);
+                        } else {
+                            throw new CustomException("Invalid input for adding employee.");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Enter Employee ID to delete:");
+                        int empIdToDelete = scanner.nextInt();
+                        deleteEmployee(empIdToDelete);
+                        break;
+                    case 3:
+                        System.out.println("Enter Employee ID to search:");
+                        int empIdToSearch = scanner.nextInt();
+                        searchEmployee(empIdToSearch);
+                        break;
+                    case 4:
+                        listEmployees();
+                        break;
+                    case 5:
+                        System.out.println("Exiting Employee Management System.");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please enter a valid option.");
+                        operations();
+                        break;
                 }
-                break;
-            case 2:
-                System.out.println("Enter Employee ID to delete:");
-                int empIdToDelete = scanner.nextInt();
-                deleteEmployee(empIdToDelete);
-                break;
-            case 3:
-                System.out.println("Enter Employee ID to search:");
-                int empIdToSearch = scanner.nextInt();
-                searchEmployee(empIdToSearch);
-                break;
-            case 4:
-                listEmployees();
-                break;
-            case 5:
-                System.out.println("Exiting Employee Management System.");
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Invalid choice. Please enter a valid option.");
+            } else {
+                System.out.println("Invalid input. Please enter a valid option.");
                 operations();
-                break;
+            }
+        } catch (CustomException e) {
+            System.out.println("Error: " + e.getMessage());
+            operations();
         }
     }
+
 
     public static void main(String[] args) {
         operations();
