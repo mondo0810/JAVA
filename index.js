d1f13ff4215f1be7f6a6d3fd5192fd4093d2ef02
MSI\thi ff vlog <thidovan58@gmail.com>
2023-10-06 08:44:43 +0700

    h

---
+++ index.js
@@ -0,0 +1,12 @@
+for (i = 0; i < 100; i++) {
+    console.log("Aaaa" + i)
+}
+for (i = 0; i < 100; i++) {
+    console.log("B" + i)
+}
+
+for (i = 0; i < 100; i++) {
+    console.log("Cccc" + i)
+}
+
+
